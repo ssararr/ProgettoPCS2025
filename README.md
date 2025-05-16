4c560ec2b38f14b17508d5a4b6e3e22d32d7159c
NiccoloRosa <s308535@studenti.polito.it>
2025-05-16 10:29:31 +0200

    Aggiunta funzione ClassificaPoliedro nel main.cpp

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+<<<<<<< Updated upstream
 # ProgettoPCS2025
+=======
+# ProgettoPCS2025
+sto modificando 
+
+
+Ciao ho scritto io sono Mattia
+Ciao io sono Nico 
+Ciao io sono Nico, no silenzio! 
+
+Ora do fastidio modificando altre cose
+
+>>>>>>> Stashed changes
--- main.cpp
+++ main.cpp
@@ -12,19 +12,21 @@ using namespace std;
 //using namespace PolyhedraLibrary;
 
 
-// La funzione prende la coppia {p, q} e una variabile booleana che serve per capire se dobbiamo fare il duale (duale = true) o no (duale = false)
-string ClassificaPoliedro(unsigned int p, unsigned int q, bool duale){
-    if(p = 3){
+// La funzione prende la coppia {p, q} e una variabile booleana che serve
+//per capire se dobbiamo fare il duale (duale = true) o no (duale = false)
+
+string ClassificaPoliedro(unsigned int& p, unsigned int& q, bool& duale){
+    if(p == 3){
         duale = false;
-        if(q = 3){
+        if(q == 3){
             return "Tetraedro";
         }
 
-        else if(q = 4){
+        else if(q == 4){
             return "Ottaedro";
         }
 
-        else if(q = 5){
+        else if(q == 5){
             return "Icosaedro";
         }
 
@@ -34,7 +36,7 @@ string ClassificaPoliedro(unsigned int p, unsigned int q, bool duale){
     }
 
     // Cubo, ma scambiamo subito p, q e ci segnamo di dover passare al duale
-    else if(p = 4 && q = 3){
+    else if(p == 4 && q == 3){
         duale = true;
         unsigned int tmp = p;
         p = q;
@@ -43,7 +45,7 @@ string ClassificaPoliedro(unsigned int p, unsigned int q, bool duale){
     }
 
     // Dodecaedro, ma scambiamo subito p, q e ci segnamo di dover passare al duale
-    else if(p = 5 && q = 3){
+    else if(p == 5 && q == 3){
         duale = true;
         unsigned int tmp = p;
         p = q;
@@ -62,7 +64,7 @@ int main(int argc, char *argv[]){
     
     // Salvo l'input nelle variabili p, q, b, c
     unsigned int arr[argc-1] = {};
-
+    bool duale  = false;
     if(argc > 1)
     {
         for(unsigned int i = 1; i < argc; i++){
@@ -78,6 +80,11 @@ int main(int argc, char *argv[]){
     unsigned int q = arr[1];
     unsigned int b = arr[2];
     unsigned int c = arr[3];
+
+    string Poliedro = ClassificaPoliedro(p, q, duale);
+
+    cout << "Poliedro: " << Poliedro << "\nDuale: " << duale << endl;
+
 
 
     // Identifico il poliedro
